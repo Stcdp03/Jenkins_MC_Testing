b92ae1dbdd3e0fcb178e1219d65103fc7656ad8c
John Vonelli <stjpv03@moravian.edu>
2017-06-08 10:28:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jenkins_MC_TESTING
---
+++ jenkins_func.py
@@ -0,0 +1,8 @@
+"""
+  This is a function that simulates Hello World
+"""
+def func(word):
+    # If the word given is Hello, it will give back World
+    if word == 'Hello':
+        return "World"
+    return 'Hello'
